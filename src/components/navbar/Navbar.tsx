2ffca5686c3006ccc7173ba308fea671810c8dd0
Raphael David <raphaelruizd@gmail.com>
2025-05-17 21:53:41 +1000

    Finish navbar component and added some svg icons

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,5 +1,38 @@
+import GithubIcon from '../icons/github-mark-white'
+import Image from 'next/image'
+import LinkedInIcon from '../icons/linkedin-icon'
+import EmailIcon from '../icons/email-icon'
+
 export default function Navbar() {
     return(
-        <h1>I'm a Navbar</h1>
+        <div className="bg-dark rounded-full">
+            <nav>
+                <div className="flex justify-between items-center p-4 space-x-12">
+                    <ul className="flex space-x-7">
+                        <li><a href="#" className="text-purple font-medium hover:text-white transition-colors">RR</a></li>
+                        <li><a href="#" className="text-purple font-medium hover:text-white transition-colors">Education</a></li>
+                        <li><a href="#" className="text-purple font-medium hover:text-white transition-colors">Experience</a></li>
+                    </ul>
+                        {/* socials */}
+                    <ul className="flex space-x-4">
+                        <li>
+                            <a href="https://www.linkedin.com/in/rr-david/" target="_blank">
+                                <LinkedInIcon className="text-purple hover:text-white transition-colors"/>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://github.com/RRdavid" target="_blank">
+                                <GithubIcon className="text-purple hover:text-white transition-colors"/>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="mailto:raphaelruizd@gmail.com">
+                                <EmailIcon className="text-purple hover:text-white transition-colors"/>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
     )
 }
