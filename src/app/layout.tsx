a7a25289711e1be34577516798a20e1199794770
Raphael David <raphaelruizd@gmail.com>
2025-06-01 15:21:41 +1000

    Adjusted global css to remove theme for dark and light for now

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${ralewaySans.variable} ${nunitoSans.variable} antialiased theme-dark`}
+        className={`${ralewaySans.variable} ${nunitoSans.variable} antialiased bg-red`}
       >
         {children}
       </body>
