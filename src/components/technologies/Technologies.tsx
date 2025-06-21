e6a980ff4ae089fd9b176ec47cab4a47df12a0ad
Raphael David <raphaelruizd@gmail.com>
2025-06-21 17:48:03 +1000

    Update Typo JS

--- src/components/technologies/Technologies.tsx
+++ src/components/technologies/Technologies.tsx
@@ -73,7 +73,7 @@ export default function Technologies({hidden}: {hidden?: boolean}) {
                         <li className="flex gap-2 p-1">
                             <span className="w-5 h-5 bg-charcoal rounded-full flex items-center justify-center">
                                 <TypeScriptIcon className="w-4 h-4 text-white" />
-                            </span>JavaScript
+                            </span>TypeScript
                         </li>
                         <li className="flex gap-2 p-1">
                             <span className="w-5 h-5 bg-charcoal rounded-full flex items-center justify-center">
