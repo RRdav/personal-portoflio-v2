b9bf0c8fe816be81d9da22c44869c68ad899d677
Raphael David <raphaelruizd@gmail.com>
2025-06-25 23:13:22 +1000

    Implemented vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Raleway, Nunito } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/footer/Footer";
+import { Analytics } from "@vercel/analytics/next";
 
 const ralewaySans = Raleway({
   variable: "--font-raleway",
@@ -30,6 +31,7 @@ export default function RootLayout({
       >
         {children}
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
